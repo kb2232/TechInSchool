7f4463fac226e0da73fb7e03719ea4d41c2c592f
kunle babatunde <kb2232@nyu.edu>
2018-08-29 21:23:05 -0400

    ui fix welcome page

--- controllers/authRoute.js
+++ controllers/authRoute.js
@@ -21,7 +21,7 @@ module.exports = function(app, passport) {
 	app.post(
 		'/login',
 		passport.authenticate('local', {
-			successRedirect: '/profile', 
+			successRedirect: '/', 
 			failureRedirect: '/login', 
 			failureFlash: true, 
 		}),
--- controllers/clientRoute.js
+++ controllers/clientRoute.js
@@ -10,16 +10,6 @@ module.exports = app => {
 		res.render('index/welcome');
 	});
 	// =====================================
-	// PROFILE SECTION =====================
-	// =====================================
-	// we will want this protected so you have to be logged in to visit
-	// we will use route middleware to verify this (the isLoggedIn function)
-	app.get('/profile', ensureAuthenticated, (req, res)=>{
-		res.render('users/profile', {
-			user: req.user, // get the user out of session and pass to template
-		});
-	});
-	// =====================================
 	// ATTENDANCE SECTION =========================
 	// =====================================
 	app.get('/attendance', ensureAuthenticated,(req, res)=>{
