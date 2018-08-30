c2259acc530d6a103970f821bb482f7cd18b4618
Michael Kim <MichaelKim3@hotmail.com>
2018-08-30 17:42:53 -0400

    Small changes for testing purposes

--- controllers/attendanceController.js
+++ controllers/attendanceController.js
@@ -9,11 +9,13 @@ var currentDate = new Date();
 // Requires confirmation that a teacher is logged in.
 // TODO: Fix and change as necessary.
 router.get('/takeAttendance', function(request, response){
+  response.render('teacher_stories/takeAttendance');
+  console.log(sessionStorage.getItem('selectedClass'));
   /* attendanceORM.allStudents("students", "classId", 3, function(result){
     var hbsObject = {
       students : result
-    }
-    response.render('teacher_stories/takeAttendance', hbsObject);
+    };
+    response.render('teacher_stories/takeAttendance', hbsObject)
   }); */
 });
 
